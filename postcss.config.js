10589ac93b06962a4a39ec417d11121475c08c53
Amruth Pillai <im.amruth@gmail.com>
2020-03-25 21:27:40 +0530

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
-
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./public/index.html', './src/**/*.js'],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
