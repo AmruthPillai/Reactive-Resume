f87b5ed6a60fd063114479d707b015f727e3a84f
Amruth Pillai <im.amruth@gmail.com>
2020-03-26 09:58:57 +0530

    update thumbnail, add hexToRgb util function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,22 @@
-/* eslint-disable import/prefer-default-export */
-
 const move = (array, element, delta) => {
   const index = array.indexOf(element);
   const newIndex = index + delta;
   if (newIndex < 0 || newIndex === array.length) return;
   const indexes = [index, newIndex].sort((a, b) => a - b);
   array.splice(indexes[0], 2, array[indexes[1]], array[indexes[0]]);
+};
+
+const hexToRgb = hex => {
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
 };
 
 const copyToClipboard = text => {
@@ -29,4 +40,4 @@ const copyToClipboard = text => {
   return successful;
 };
 
-export { move, copyToClipboard };
+export { move, hexToRgb, copyToClipboard };
