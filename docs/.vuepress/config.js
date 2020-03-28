29abae8c5ea9afd51d25a9b0c3519741a2fd7951
Amruth Pillai <im.amruth@gmail.com>
2020-03-28 20:28:35 +0530

    add features and built with pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,12 @@ module.exports = {
   description:
     "A one-of-a-kind resume builder that's not out to get your data. Completely secure, customizable, portable, open-source and free forever.",
   themeConfig: {
-    logo: 'logo.png',
+    logo: '/logo.png',
+    repo: 'AmruthPillai/Reactive-Resume',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Go to App', link: 'https://rxresu.me/' },
     ],
+    sidebar: ['/', '/features/', '/built-with/'],
   },
 };
---
+++ docs/built-with/README.md
@@ -0,0 +1,8 @@
+---
+title: Built With
+---
+
+# Built With
+
+- React
+- Tailwind CSS
