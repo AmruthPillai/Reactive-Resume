e25c0b302bc44813af671ce4783968628de9dcc1
Amruth Pillai <im.amruth@gmail.com>
2020-04-01 15:54:53 +0530

    fix eslint warnings

--- src/components/LeftSidebar/tabs/Skills.js
+++ src/components/LeftSidebar/tabs/Skills.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from 'react';
-import { useTranslation } from 'react-i18next';
 
 import AppContext from '../../../context/AppContext';
 import Checkbox from '../../../shared/Checkbox';
