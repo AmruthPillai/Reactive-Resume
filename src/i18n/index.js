92b00db93260a69f8f0fa483458e996cc2cb350f
Amruth Pillai <im.amruth@gmail.com>
2020-04-11 20:19:16 +0530

    add language detector from i18next

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,5 +1,7 @@
 import i18n from 'i18next';
+import backend from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
+import detector from 'i18next-browser-languagedetector';
 
 import resources from './locales';
 
@@ -46,13 +48,17 @@ const languages = [
   },
 ];
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'en',
-  fallbackLng: 'en',
-  ns: ['app', 'leftSidebar', 'rightSidebar'],
-  defaultNS: 'app',
-});
+i18n
+  .use(detector)
+  .use(backend)
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    ns: ['app', 'leftSidebar', 'rightSidebar'],
+    defaultNS: 'app',
+  });
 
 export { languages };
 
