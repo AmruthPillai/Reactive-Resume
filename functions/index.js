c7b39c9ed3cfeaba55ef192c72c669098d718624
Amruth Pillai <im.amruth@gmail.com>
2020-07-13 15:58:59 +0530

    - updated packages across projects, deployed to staging

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,20 @@
-const functions = require("firebase-functions");
-const puppeteer = require("puppeteer");
+const functions = require('firebase-functions');
+const puppeteer = require('puppeteer');
+const cors = require('cors')({ origin: 'https://rxresume-staging.web.app' });
 
-require('dotenv').config();
-const cors = require("cors")({
-  origin: true,
-});
-
-const BASE_URL =  process.env.SITE_URL + "/r/";
+const BASE_URL = 'https:/rxresu.me/r/';
 
 function timeout(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 exports.printSinglePageResume = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    return res.status(403).send("Forbidden!");
+  if (req.method !== 'POST') {
+    return res.status(403).send('Forbidden!');
   }
 
   if (!req.query.id) {
-    return res.status(400).send("Bad Request!");
+    return res.status(400).send('Bad Request!');
   }
 
   async function run() {
@@ -26,7 +22,7 @@ exports.printSinglePageResume = functions.https.onRequest((req, res) => {
     const page = await browser.newPage();
     await page.goto(BASE_URL + req.query.id);
     await timeout(5000);
-    await page.emulateMediaType("print");
+    await page.emulateMediaType('print');
     const height = await page.evaluate(() => {
       var body = document.body,
         html = document.documentElement;
@@ -36,7 +32,7 @@ exports.printSinglePageResume = functions.https.onRequest((req, res) => {
         body.offsetHeight,
         html.clientHeight,
         html.scrollHeight,
-        html.offsetHeight
+        html.offsetHeight,
       );
 
       return height;
@@ -45,7 +41,7 @@ exports.printSinglePageResume = functions.https.onRequest((req, res) => {
       printBackground: true,
       width: `21cm`,
       height: `${height}px`,
-      pageRanges: "1",
+      pageRanges: '1',
     });
     await browser.close();
     return pdf;
@@ -54,20 +50,20 @@ exports.printSinglePageResume = functions.https.onRequest((req, res) => {
   return cors(req, res, async () => {
     const pdf = await run();
     res.set({
-      "Content-Type": "application/pdf",
-      "Content-Length": pdf.length,
+      'Content-Type': 'application/pdf',
+      'Content-Length': pdf.length,
     });
     return res.send(pdf);
   });
 });
 
 exports.printMultiPageResume = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    return res.status(403).send("Forbidden!");
+  if (req.method !== 'POST') {
+    return res.status(403).send('Forbidden!');
   }
 
   if (!req.query.id) {
-    return res.status(400).send("Bad Request!");
+    return res.status(400).send('Bad Request!');
   }
 
   async function run() {
@@ -75,7 +71,7 @@ exports.printMultiPageResume = functions.https.onRequest((req, res) => {
     const page = await browser.newPage();
     await page.goto(BASE_URL + req.query.id);
     await timeout(5000);
-    await page.emulateMediaType("print");
+    await page.emulateMediaType('print');
     const pdf = await page.pdf({
       printBackground: true,
       width: `21cm`,
@@ -87,8 +83,8 @@ exports.printMultiPageResume = functions.https.onRequest((req, res) => {
   return cors(req, res, async () => {
     const pdf = await run();
     res.set({
-      "Content-Type": "application/pdf",
-      "Content-Length": pdf.length,
+      'Content-Type': 'application/pdf',
+      'Content-Length': pdf.length,
     });
     return res.send(pdf);
   });
