3a2f9c1542dc27b70c87bbca2d755a227fa16473
Amruth Pillai <im.amruth@gmail.com>
2020-07-17 10:54:10 +0530

    - update version

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Reactive Resume',
     siteUrl: 'https://rxresu.me',
     description: 'A free and open source resume builder.',
-    version: '2.0.0',
+    version: '2.0.1',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
