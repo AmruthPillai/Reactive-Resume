f1fe45e9c9c6d68d9f5b391da4fd4ae3f86922cf
Amruth Pillai <im.amruth@gmail.com>
2020-07-24 21:56:02 +0530

    removed experimental code

--- src/templates/Castform.js
+++ src/templates/Castform.js
@@ -57,7 +57,6 @@ const Castform = ({ data }) => {
         id="page"
         className="rounded"
         style={{
-          fontSize: 14,
           fontFamily: data.metadata.font,
           color: data.metadata.colors.text,
           backgroundColor: data.metadata.colors.background,
