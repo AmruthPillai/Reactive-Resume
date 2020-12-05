bce80a9101195a1c826738a0f7ce7f996305f1e2
Amruth Pillai <amruthpillai@Amruths-MacBook-Pro.local>
2020-12-05 16:19:52 +0530

    bump up version to 2.3

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Reactive Resume',
     siteUrl: 'https://rxresu.me',
     description: 'A free and open source resume builder.',
-    version: '2.2.1',
+    version: '2.3',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
