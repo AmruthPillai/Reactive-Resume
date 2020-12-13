96ab340b1c6f7859843032a6d8966785490546ad
Amruth Pillai <im.amruth@gmail.com>
2020-12-13 10:37:02 +0530

    set min-height for pages when printing

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Reactive Resume',
     siteUrl: 'https://rxresu.me',
     description: 'A free and open source resume builder.',
-    version: '2.3.4',
+    version: '2.3.5',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
