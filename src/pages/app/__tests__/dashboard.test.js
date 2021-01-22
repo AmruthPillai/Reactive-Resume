cb2aeec70a4d34f5f758fc40920269d8222b88bb
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2021-01-22 10:43:06 +0100

    Dashboard: added unit tests for Delete resume

--- src/pages/app/__tests__/dashboard.test.js
+++ src/pages/app/__tests__/dashboard.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {
+  fireEvent,
+  queryByText,
   render,
   screen,
+  waitFor,
   waitForElementToBeRemoved,
 } from '@testing-library/react';
 
@@ -10,6 +13,7 @@ import FirebaseStub, { DatabaseConstants } from 'gatsby-plugin-firebase';
 import '../../../i18n/index';
 import '../../../utils/dayjs';
 import { dataTestId as loadingScreenTestId } from '../../../components/router/LoadingScreen';
+import { menuToggleDataTestIdPrefix as resumePreviewMenuToggleDataTestIdPrefix } from '../../../components/dashboard/ResumePreview';
 import { SettingsProvider } from '../../../contexts/SettingsContext';
 import { ModalProvider } from '../../../contexts/ModalContext';
 import { UserProvider } from '../../../contexts/UserContext';
@@ -82,6 +86,68 @@ describe('Dashboard', () => {
       expect(
         screen.getByText(Object.values(resumes)[1].name),
       ).toBeInTheDocument();
+    });
+  });
+
+  describe('when resume is deleted', () => {
+    let mockDatabaseRemoveFunction = null;
+    let resumeToDelete = null;
+    let undeletedResume = null;
+    let resumeToDeleteId = null;
+
+    beforeEach(async () => {
+      await setup();
+
+      [resumeToDelete, undeletedResume] = Object.values(resumes);
+      resumeToDeleteId = resumeToDelete.id;
+
+      mockDatabaseRemoveFunction = jest.spyOn(
+        FirebaseStub.database().ref(
+          `${DatabaseConstants.resumesPath}/${resumeToDeleteId}`,
+        ),
+        'remove',
+      );
+
+      const resumeToDeleteMenuToggle = screen.getByTestId(
+        `${resumePreviewMenuToggleDataTestIdPrefix}${resumeToDeleteId}`,
+      );
+      fireEvent.click(resumeToDeleteMenuToggle);
+
+      const menuItems = screen.getAllByRole('menuitem');
+      let deleteMenuItem = null;
+      for (let index = 0; index < menuItems.length; index++) {
+        if (queryByText(menuItems[index], /delete/i)) {
+          deleteMenuItem = menuItems[index];
+          break;
+        }
+      }
+      fireEvent.click(deleteMenuItem);
+    });
+
+    it('removes it from database and preview', async () => {
+      await waitFor(() =>
+        expect(mockDatabaseRemoveFunction).toHaveBeenCalledTimes(1),
+      );
+
+      await waitFor(() =>
+        expect(screen.queryByText(resumeToDelete.name)).toBeNull(),
+      );
+      expect(screen.getByText(undeletedResume.name)).toBeInTheDocument();
+    });
+
+    /*
+    it('displays notification', async () => {
+      await waitFor(() =>
+        expect(
+          screen.getByText(`${resumeToDelete.name} was deleted successfully`),
+        ).toBeInTheDocument(),
+      );
+    });
+    */
+
+    it('closes menu', () => {
+      const menuItems = screen.queryAllByRole('menuitem');
+      expect(menuItems).toHaveLength(0);
     });
   });
 
