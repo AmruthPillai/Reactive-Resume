22a171c25c51bbada0db548b44c4052df5df9610
AmruthPillai <im.amruth@gmail.com>
2021-01-23 19:44:20 +0530

    [2.5.4] Add Tests, Add GitHub Action, Update Dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'Reactive Resume',
     siteUrl: 'https://rxresu.me',
     description: 'A free and open source resume builder.',
-    version: '2.5.3',
+    version: '2.5.4',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
