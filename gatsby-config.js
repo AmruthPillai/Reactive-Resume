c6986b5048d9c828fcc003d738cc759aeb2a28de
Amruth Pillai <im.amruth@gmail.com>
2021-06-22 16:53:37 +0530

    v2.7.5 - add Indonesian language

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Reactive Resume',
     siteUrl: 'https://rxresu.me',
     description: 'A free and open source resume builder.',
-    version: '2.7.4',
+    version: '2.7.5',
   },
   flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: [
