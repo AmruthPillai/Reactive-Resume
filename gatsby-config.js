6754cb1e4595c6d27df4df30f18363dd592a6858
Amruth Pillai <im.amruth@gmail.com>
2021-06-26 21:21:28 +0530

    bump verison to v2.7.7, translations added

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Reactive Resume',
     siteUrl: 'https://rxresu.me',
     description: 'A free and open source resume builder.',
-    version: '2.7.6',
+    version: '2.7.7',
   },
   flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: [
