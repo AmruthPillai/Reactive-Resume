43c09666a038a956f91fb0c668f329cc10f5f9e8
Amruth Pillai <im.amruth@gmail.com>
2022-11-24 11:42:03 +0100

    add sentry CLI to github actions

--- client/sentry.server.config.js
+++ client/sentry.server.config.js
@@ -1,7 +1,11 @@
 import env from '@beam-australia/react-env';
 import * as Sentry from '@sentry/nextjs';
 
+const sentryDSN =
+  env('CLIENT_SENTRY_DSN') ||
+  'https://aceffdbdaa544768bc85216e7c6a9c50@o4504211187564544.ingest.sentry.io/4504213380071424';
+
 Sentry.init({
-  dsn: env('CLIENT_SENTRY_DSN'),
+  dsn: sentryDSN,
   tracesSampleRate: 1.0,
 });
