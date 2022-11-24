6ee290a625f4f0da7c366b1abcb226be96d287e1
Amruth Pillai <im.amruth@gmail.com>
2022-11-24 21:31:43 +0100

    add logs to check what's wrong

--- client/pages/[username]/[slug]/index.tsx
+++ client/pages/[username]/[slug]/index.tsx
@@ -39,6 +39,8 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ query, loc
   try {
     const resume = await fetchResumeByIdentifier({ username, slug });
 
+    console.log('Preview~getServerSideProps~resume', resume);
+
     return {
       props: { username, slug, resume, ...(await serverSideTranslations(locale, ['common'])) },
     };
@@ -61,6 +63,8 @@ const Preview: NextPage<Props> = ({ username, slug, resume: initialData }) => {
   }, [dispatch, initialData]);
 
   useEffect(() => {
+    console.log('Preview~useEffect~resume', resume);
+
     const locale = get(resume, 'metadata.locale', 'en');
 
     if (!isEmpty(resume) && router.locale !== locale) {
