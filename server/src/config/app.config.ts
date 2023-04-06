b4381a22f337bf40dd79a47f6026246d7edd6deb
Amruth Pillai <im.amruth@gmail.com>
2023-04-06 15:03:53 +0200

    remove VERSION env dependency from server

--- server/src/config/app.config.ts
+++ server/src/config/app.config.ts
@@ -2,7 +2,6 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   timezone: process.env.TZ,
-  version: process.env.VERSION,
   environment: process.env.NODE_ENV,
   secretKey: process.env.SECRET_KEY,
   port: parseInt(process.env.PORT, 10) || 3100,
