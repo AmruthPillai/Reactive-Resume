5ee99cfdab15e2c75bf057c359c37e6c4a022ff6
Amruth Pillai <im.amruth@gmail.com>
2023-06-07 12:06:04 +0200

    attempt to fix internal server error

--- server/src/config/app.config.ts
+++ server/src/config/app.config.ts
@@ -5,6 +5,6 @@ export default registerAs('app', () => ({
   environment: process.env.NODE_ENV,
   secretKey: process.env.SECRET_KEY,
   port: parseInt(process.env.PORT, 10) || 3100,
-  url: process.env.PUBLIC_URL || 'http://localhost:3000',
-  serverUrl: process.env.PUBLIC_SERVER_URL || 'http://localhost:3100',
+  url: process.env.PUBLIC_URL || 'http://localhost:3000/',
+  serverUrl: process.env.PUBLIC_SERVER_URL || 'http://localhost:3100/',
 }));
