c4d3dd89d77c0edb6534a0dec2f6fbc69436284e
Amruth Pillai <im.amruth@gmail.com>
2023-11-24 01:07:02 +0100

    feat: add product hunt banner to landing page

--- apps/client/src/libs/dayjs.ts
+++ apps/client/src/libs/dayjs.ts
@@ -1,9 +1,13 @@
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import relativeTime from "dayjs/plugin/relativeTime";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 
 dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
+dayjs.extend(timezone);
+dayjs.extend(utc);
 
 export const dayjsLocales: Record<string, () => Promise<ILocale>> = {
   "af-ZA": () => import("dayjs/locale/af"),
