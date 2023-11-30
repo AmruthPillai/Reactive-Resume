fdf0bba900d639833c44fed9821ff0518e1b72f5
Anubhav Singh <anubhav.singh.e@thalesdigital.io>
2023-11-30 22:27:18 +0530

    On Dashboard fix options dropdown on Both Card

--- apps/client/src/pages/dashboard/resumes/_layouts/grid/_components/resume-card.tsx
+++ apps/client/src/pages/dashboard/resumes/_layouts/grid/_components/resume-card.tsx
@@ -2,6 +2,7 @@ import { t } from "@lingui/macro";
 import {
   CircleNotch,
   CopySimple,
+  DotsThreeVertical,
   FolderOpen,
   Lock,
   LockOpen,
@@ -10,11 +11,16 @@ import {
 } from "@phosphor-icons/react";
 import { ResumeDto } from "@reactive-resume/dto";
 import {
+  Button,
   ContextMenu,
   ContextMenuContent,
   ContextMenuItem,
   ContextMenuSeparator,
   ContextMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
 } from "@reactive-resume/ui";
 import { cn } from "@reactive-resume/utils";
 import dayjs from "dayjs";
@@ -58,6 +64,77 @@ export const ResumeCard = ({ resume }: Props) => {
   const onDelete = () => {
     open("delete", { id: "resume", item: resume });
   };
+
+  const dropdownMenu = (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="aspect-square">
+        <Button size="icon" variant="ghost">
+          <DotsThreeVertical />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem
+          onClick={(event) => {
+            event.stopPropagation();
+            onOpen();
+          }}
+        >
+          <FolderOpen size={14} className="mr-2" />
+          {t`Open`}
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={(event) => {
+            event.stopPropagation();
+            onUpdate();
+          }}
+        >
+          <PencilSimple size={14} className="mr-2" />
+          {t`Rename`}
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={(event) => {
+            event.stopPropagation();
+            onDuplicate();
+          }}
+        >
+          <CopySimple size={14} className="mr-2" />
+          {t`Duplicate`}
+        </DropdownMenuItem>
+        {resume.locked ? (
+          <DropdownMenuItem
+            onClick={(event) => {
+              event.stopPropagation();
+              onLockChange();
+            }}
+          >
+            <LockOpen size={14} className="mr-2" />
+            {t`Unlock`}
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem
+            onClick={(event) => {
+              event.stopPropagation();
+              onLockChange();
+            }}
+          >
+            <Lock size={14} className="mr-2" />
+            {t`Lock`}
+          </DropdownMenuItem>
+        )}
+        <ContextMenuSeparator />
+        <DropdownMenuItem
+          className="text-error"
+          onClick={(event) => {
+            event.stopPropagation();
+            onDelete();
+          }}
+        >
+          <TrashSimple size={14} className="mr-2" />
+          {t`Delete`}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 
   return (
     <ContextMenu>
@@ -108,12 +185,15 @@ export const ResumeCard = ({ resume }: Props) => {
 
           <div
             className={cn(
-              "absolute inset-x-0 bottom-0 z-10 flex flex-col justify-end space-y-0.5 p-4 pt-12",
+              "absolute inset-x-0 bottom-0 z-10 flex flex-row justify-between space-y-0.5 p-4 pt-12",
               "bg-gradient-to-t from-background/80 to-transparent",
             )}
           >
-            <h4 className="line-clamp-2 font-medium">{resume.title}</h4>
-            <p className="line-clamp-1 text-xs opacity-75">{t`Last updated ${lastUpdated}`}</p>
+            <div>
+              <h4 className="line-clamp-2 font-medium">{resume.title}</h4>
+              <p className="line-clamp-1 justify-end text-xs opacity-75">{t`Last updated ${lastUpdated}`}</p>
+            </div>
+            <div>{dropdownMenu}</div>
           </div>
         </BaseCard>
       </ContextMenuTrigger>
