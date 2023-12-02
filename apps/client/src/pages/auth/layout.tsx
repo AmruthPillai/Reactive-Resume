d061829bebe355d9800801f5b1150be4ed3f4aad
MYSTYX7 <cool.abhi@outlook.com>
2023-12-02 23:59:55 +0530

    fix(auth): Fix Locale & ThemeSwitch button overlap

--- apps/client/src/pages/auth/layout.tsx
+++ apps/client/src/pages/auth/layout.tsx
@@ -24,7 +24,7 @@ export const AuthLayout = () => {
 
   return (
     <div className="flex h-screen w-screen">
-      <div className="relative flex w-full flex-col justify-center -mt-12 gap-y-8 px-12 sm:mx-auto sm:basis-[420px] sm:px-0 lg:basis-[480px] lg:px-12">
+      <div className="relative flex w-full flex-col justify-center -mt-8 gap-y-8 px-12 sm:mx-auto sm:basis-[420px] sm:px-0 lg:basis-[480px] lg:px-12">
         <div className="flex items-center justify-between">
           <Link to="/" className="h-24 w-24">
             <Logo className="-ml-3" size={96} />
