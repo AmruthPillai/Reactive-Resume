b8c5aafa1de96e7e7a063563b58c91e1b0f7b36b
Miguel Medina <miguel@medinagtz.com>
2023-12-03 19:59:01 -0600

    Add option to skip storage bucket check & creation in storage service

--- apps/server/src/storage/storage.service.ts
+++ apps/server/src/storage/storage.service.ts
@@ -44,6 +44,8 @@ export class StorageService implements OnModuleInit {
   private client: Client;
   private bucketName: string;
 
+  private skipCreateBucket: boolean;
+
   constructor(
     private readonly configService: ConfigService<Config>,
     private readonly minioService: MinioService,
@@ -55,6 +57,12 @@ export class StorageService implements OnModuleInit {
   async onModuleInit() {
     this.client = this.minioService.client;
     this.bucketName = this.configService.getOrThrow<string>("STORAGE_BUCKET");
+    this.skipCreateBucket = this.configService.getOrThrow<boolean>("STORAGE_SKIP_CREATE_BUCKET");
+
+    if (this.skipCreateBucket) {
+      this.logger.log("Skipping the creation of the storage bucket.");
+      return;
+    }
 
     try {
       // Create a storage bucket if it doesn't exist
