a095cb8255ee4665789bbd18be7d99ca9f48eb58
juliandaz11 <55860818+juliandaz11@users.noreply.github.com>
2023-12-14 23:16:53 +0000

    Update fix-grammar.ts
    returns in the language of the text

--- apps/client/src/services/openai/fix-grammar.ts
+++ apps/client/src/services/openai/fix-grammar.ts
@@ -6,7 +6,7 @@ import { openai } from "./client";
 
 const PROMPT = `You are an AI writing assistant specialized in writing copy for resumes.
 Do not return anything else except the text you improved. It should not begin with a newline. It should not have any prefix or suffix text.
-Just fix the spelling and grammar of the following paragraph, do not change the meaning:
+Just fix the spelling and grammar of the following paragraph, do not change the meaning and returns in the language of the text:
 
 Text: """{input}"""
 
