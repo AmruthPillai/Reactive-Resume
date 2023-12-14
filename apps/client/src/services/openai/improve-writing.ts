51408eb03ceb4662e1589226559a54c1ca74be8c
juliandaz11 <55860818+juliandaz11@users.noreply.github.com>
2023-12-14 23:12:59 +0000

    Update improve-writing.ts
    returns in the language of the text

--- apps/client/src/services/openai/improve-writing.ts
+++ apps/client/src/services/openai/improve-writing.ts
@@ -6,7 +6,7 @@ import { openai } from "./client";
 
 const PROMPT = `You are an AI writing assistant specialized in writing copy for resumes.
 Do not return anything else except the text you improved. It should not begin with a newline. It should not have any prefix or suffix text.
-Improve the writing of the following paragraph:
+Improve the writing of the following paragraph and returns in the language of the text:
 
 Text: """{input}"""
 
