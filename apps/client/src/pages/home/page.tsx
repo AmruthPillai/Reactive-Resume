0097d76c35cf64ae12556221185e42359d15b5a0
Anubhav Singh <anubhav.singh.e@thalesdigital.io>
2023-12-30 15:38:18 +0530

    hoem

--- apps/client/src/pages/home/page.tsx
+++ apps/client/src/pages/home/page.tsx
@@ -35,7 +35,7 @@ export const HomePage = () => {
       <FeaturesSection />
       <TemplatesSection />
       <KeywordsSection />
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
       {/* <SupportSection /> */}
       <FAQSection />
       {/* <ContributorsSection /> */}
