76272f27685ba42b91ac3c31184add09460a018c
Anubhav Singh <anubhav.singh.e@thalesdigital.io>
2024-01-12 03:33:04 +0530

    Drag icon fixed

--- libs/ui/src/components/resizable-panel.tsx
+++ libs/ui/src/components/resizable-panel.tsx
@@ -30,8 +30,8 @@ export const PanelResizeHandle = ({
       />
     </div>
 
-    <div className="pointer-events-none absolute inset-y-0 left-[-5px] z-50 flex items-center justify-center">
-      <DotsSixVertical size={14} opacity={0.75} />
+    <div className="pointer-events-none absolute inset-y-0 left-[-5px] z-50 flex items-center justify-center bg-secondary opacity-100">
+      <DotsSixVertical size={14} opacity={1} />
     </div>
   </PanelPrimitive.PanelResizeHandle>
 );
