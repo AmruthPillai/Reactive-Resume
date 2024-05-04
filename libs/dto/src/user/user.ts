35597f8c2d545ab71272d6bdd1da296db0da4c36
sachinninave <ninavesachin32@gmail.com>
2024-05-04 07:18:30 +0530

    zod validation for user

--- libs/dto/src/user/user.ts
+++ libs/dto/src/user/user.ts
@@ -18,7 +18,7 @@ export const userSchema = z.object({
   name: z.string().min(1).max(255),
   picture: z.literal("").or(z.null()).or(z.string().url()),
   username: usernameSchema,
-  email: z.string().email(),
+  email: z.string().email().toLowerCase(),
   locale: z.string().default("en-US"),
   emailVerified: z.boolean().default(false),
   twoFactorEnabled: z.boolean().default(false),
