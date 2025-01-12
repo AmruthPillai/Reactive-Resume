89a44cc33a846d13f088902287b902949688d663
Amruth Pillai <im.amruth@gmail.com>
2025-01-12 15:13:11 +0100

    - normalize username and email fields to lowercase, resolves #1740 - add autoComplete attributes to auth flow for easier sign in/sign up

--- libs/dto/src/user/user.ts
+++ libs/dto/src/user/user.ts
@@ -8,17 +8,20 @@ export const usernameSchema = z
   .string()
   .min(3)
   .max(255)
-  .regex(/^[\d._a-z-]+$/, {
-    message:
-      "Usernames can only contain lowercase letters, numbers, periods, hyphens, and underscores.",
-  });
+  .regex(/^[\w.-]+$/, {
+    message: "Usernames can only contain letters, numbers, periods, hyphens, and underscores.",
+  })
+  .transform((value) => value.toLowerCase());
 
 export const userSchema = z.object({
   id: idSchema,
   name: z.string().min(1).max(255),
   picture: z.literal("").or(z.null()).or(z.string().url()),
   username: usernameSchema,
-  email: z.string().email(),
+  email: z
+    .string()
+    .email()
+    .transform((value) => value.toLowerCase()),
   locale: z.string().default("en-US"),
   emailVerified: z.boolean().default(false),
   twoFactorEnabled: z.boolean().default(false),
