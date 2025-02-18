f7876f54c314b84ba38a77a167c6ee09df9f10fb
Mads Knudsen <madsfknudsen@hotmail.com>
2025-02-18 15:33:17 +0100

    Update create.ts

--- apps/client/src/services/section/create.ts
+++ apps/client/src/services/section/create.ts
@@ -3,12 +3,10 @@ import { CreateSectionDto, SectionDto } from "@reactive-resume/dto";
 import { AxiosResponse } from "axios";
 
 export const createSection = async (data: CreateSectionDto) => {
-  console.log("Sending data:", data);
   const response = await axios.post<SectionDto, AxiosResponse<SectionDto>, CreateSectionDto>(
     "/section",
     data,
   );
 
-  console.log("Received data:",response.data);
   return response.data;
 };
