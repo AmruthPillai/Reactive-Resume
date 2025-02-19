e432c358b4884eba0a458e943d2639d3a8202995
Mads Knudsen <madsfknudsen@hotmail.com>
2025-02-19 10:48:58 +0100

    Update delete.ts

--- apps/client/src/services/section/delete.ts
+++ apps/client/src/services/section/delete.ts
@@ -2,6 +2,6 @@ import { DeleteDto } from "@reactive-resume/dto";
 import { axios } from "@/client/libs/axios";
 
 export const deleteSectionItem = async (data: DeleteDto) => {
-  const response = await axios.delete(`/sections/${data.id}`);
+  const response = await axios.delete(`/sectionItem/${data.id}`);
   return response.data as DeleteDto;
 };
