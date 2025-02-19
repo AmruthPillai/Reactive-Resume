66232176a20c87a4dd9796d30e313b88de7579a6
krisdanoda <iluvmizuki@gmail.com>
2025-02-19 10:22:35 +0100

    hot fixing main
    Fixing the optional error

--- tools/prisma/migrations/20250217113041_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,id]` on the table `Section` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Section_userId_id_key" ON "Section"("userId", "id");
