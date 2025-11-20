03e08b456ffa1bddec48c9af91047975b7474024
Abderrahamane <anizi.dev@gmail.com>
2025-11-20 17:30:32 +0100

    refactor(client-folder): Change zod import syntax

--- libs/dto/src/folder/update.ts
+++ libs/dto/src/folder/update.ts
@@ -1,6 +1,6 @@
 import { idSchema } from "@reactive-resume/schema";
 import { createZodDto } from "nestjs-zod/dto";
-import z from "zod";
+import { z } from "zod";
 
 const updateFolderSchema = z.object({
   id: idSchema.optional(),
